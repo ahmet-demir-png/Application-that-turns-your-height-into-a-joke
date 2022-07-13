992ca8f3c4c7f9a844c6a67734e73e320bc129fe
OPTER <77252619+ahmet-demir-png@users.noreply.github.com>
2022-07-13 21:35:12 +0300

    Add project files.

---
+++ Boyunu tahmi eden uygulama/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Boyunu_tahmi_eden_uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.anabutton1 = new System.Windows.Forms.Button();
+            this.anatext1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // anabutton1
+            // 
+            this.anabutton1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.anabutton1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.anabutton1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.anabutton1.Location = new System.Drawing.Point(240, 215);
+            this.anabutton1.Name = "anabutton1";
+            this.anabutton1.Size = new System.Drawing.Size(328, 80);
+            this.anabutton1.TabIndex = 0;
+            this.anabutton1.Text = "Hazır mısın?";
+            this.anabutton1.UseVisualStyleBackColor = false;
+            this.anabutton1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.anabutton1_MouseClick);
+            // 
+            // anatext1
+            // 
+            this.anatext1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.anatext1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.anatext1.Font = new System.Drawing.Font("Segoe UI", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.anatext1.ForeColor = System.Drawing.SystemColors.Window;
+            this.anatext1.Location = new System.Drawing.Point(240, 147);
+            this.anatext1.Name = "anatext1";
+            this.anatext1.Size = new System.Drawing.Size(328, 44);
+            this.anatext1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(240, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Boyunuzu yazın örnek(1.80)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.anatext1);
+            this.Controls.Add(this.anabutton1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Boyunu tahmin edecek inanır mısın?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button anabutton1;
+        public System.Windows.Forms.TextBox anatext1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Boyunu tahmi eden uygulama/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Boyunu_tahmi_eden_uygulama
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(148, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Boyunuz";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::Boyunu_tahmi_eden_uygulama.Properties.Resources._0f79dcaf8bf5aa9041dfcbcb2340571b;
+            this.ClientSize = new System.Drawing.Size(481, 413);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "OHHH HAY MAŞALLAH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+    }
+}
